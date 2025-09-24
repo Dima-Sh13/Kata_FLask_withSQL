28a46157cf8004496b5ee876400c0b0fd4a5dd77
Dima Shalupnya <dshalupnya@gmail.com>
2025-09-24 19:29:38 +0200

    Refactorization of code and addition of all templates

---
+++ app/models.py
@@ -0,0 +1,42 @@
+import sqlite3
+from app import ORIGIN_DATA
+
+def select_all():
+    conexion = sqlite3.connect(ORIGIN_DATA)
+    cur = conexion.cursor()
+    res = cur.execute("SELECT * from movement;")
+    filas = res.fetchall()#datos de columnas (2025-09-01, Nomina,1800),(2025-09-05,Mercado,-100)
+    columnas = res.description#nombre de columnas en las primeras filas (id,0000),(date,000)
+
+    lista_diccionario= []
+    
+    for f in filas:
+        posicion = 0
+        diccionario = {}
+        for c in columnas:
+            diccionario[c[0]] =  f[posicion]
+            posicion+=1
+        lista_diccionario.append(diccionario)
+
+    conexion.close()
+    
+    return lista_diccionario
+
+def insert(registroForm):
+    conexion = sqlite3.connect(ORIGIN_DATA)
+    cur = conexion.cursor()
+    res = cur.execute("INSERT INTO movement (date, concept, quantity) VALUES (?,?,?);",registroForm)
+    conexion.commit()#funcion para validar el registro antes de guardarlo
+
+    conexion.close()
+
+def select_by(id):
+    conexion= sqlite3.connect(ORIGIN_DATA)
+    cur = conexion.cursor()
+    res = cur.execute(f"SELECT * from movement WHERE id={id};")
+    result = res.fetchall()
+    return result[0]
+
+
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,51 @@
 from app import app
+from flask import render_template,request,redirect
+from app.models import *
+from datetime import date
 
-from flask import render_template, request
+def validarFormulario(datosFormulario):
+    errores = []#se crea la lista para guardar errores
+    hoy = str(date.today())
+    if datosFormulario['date'] > hoy:
+        errores.append("La fecha no puede ser mayor a la actual")
+    if datosFormulario['concept'] == "":
+        errores.append("El concepto no puede ir vacio")
+    if datosFormulario['quantity'] == "" or float(datosFormulario['quantity']) == 0.0:
+        errores.append("El monto debe ser distinto de 0 y de vacio")
+
+    return errores
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    diccionario = select_all()
+
+    return render_template("index.html",lista=diccionario)
+
+@app.route("/new",methods=["GET","POST"])
+def create():
+    if request.method == "GET":#GET
+        return render_template("create.html",dataForm={})
+    else:#POST
+        errores = validarFormulario(request.form)
+        if errores:
+            return render_template("create.html",errors=errores,dataForm=request.form)
+        insert( [ request.form['date'],request.form['concept'],request.form['quantity'] ])
+
+        return redirect("/")
+    
+@app.route("/delete/<int:id>",methods=["GET","POST"])
+def remove(id):
+    if request.method == "GET":#get
+        resultado = select_by(id)
+        return render_template("delete.html",data=resultado)
+    else:#post
+        return f"registro para eliminar con id:{id}"
+    
+@app.route("/update/<int:id>",methods=["GET","POST"])
+def update(id):
+    if request.method == "GET": #get
+        resultado = select_by(id)
+        return render_template("update.html",datos=resultado)
+    else:#post
+        return f"registro para actualizar con el id:{id}"    
